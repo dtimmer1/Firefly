5270b0be8c9381790355739086a13ee17dbfa52b
David Timmerman <dtimmer1@binghamton.edu>
2021-11-01 17:58:04 -0400

    fixed partitioning bug and added comments

--- weather_parser.go
+++ weather_parser.go
@@ -3,22 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
+
+/*
+ * readWeather()
+ * Process weather data for a chunk of the search area.
+ * Launches a single goroutine that scans the region defined by lat0, lat1, long0, and long1,
+ * sending GET requests to each 2.5x2.5km grid square.
+ * Sends the output to the main goroutine via a generated channel.
+ */
 
 func readWeather(lat0 float64, lat1 float64, long0 float64, long1 float64) <-chan int {
 	c := make(chan int)
 	go func() {
 		total := 0
-		for lat := lat0; lat < lat1; lat += 0.05 {
-			for long := long0; long < long1; long += 0.05 {
+		for lat := lat0; lat < lat1; lat += 0.02 { //2.5km is roughly 0.02 degrees of latitude/longitude
+			for long := long0; long < long1; long += 0.02 {
 				url := fmt.Sprintf("https://api.weather.gov/points/%f,%f", lat, long)
-				val, err := http.Get(url)
+				val, err := http.Get(url) //val contains the body of the HTTP request, err contains any error code
 				if err == nil && val != nil {
-					fmt.Println(url)
-					total += 1
+					total += 1 //for debugging purposes we're just returning the number of processed regions
 				} else {
 					fmt.Println("error!")
 				}
+				time.Sleep(time.Millisecond * time.Duration(100))
 			}
 		}
 		c <- total
@@ -29,21 +38,20 @@ func readWeather(lat0 float64, lat1 float64, long0 float64, long1 float64) <-cha
 
 func main() {
 
-	var outputs []<-chan int
+	var outputs []<-chan int //array containing receive-only int channels
 
-	lat0, lat1, long0, long1 := 30., 31., -110., -109.
-	latInterval := (lat1 - lat0) / 10
-	longInterval := (long1 - long0) / 10
+	lat0, lat1, long0, long1 := 40.97, 44.98, -111.03, -104.06
+	latInterval := (lat1 - lat0) / 50
 
-	for i := 0; i < 10; i++ {
-		outputs = append(outputs, readWeather(lat0, lat0+latInterval, long0, long0+longInterval))
+	for i := 0; i < 50; i++ {
+		channel := readWeather(lat0, lat0+latInterval, long0, long1)
+		outputs = append(outputs, channel) //appends to the array
 		lat0 += latInterval
-		long0 += longInterval
 	}
 
 	total := 0
 
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 50; i++ {
 		total += <-outputs[i]
 	}
 
